f91f34f479069090c17c067d32467569e2a85f42
awesomels <liushan201319@gmail.com>
2016-08-05 11:04:28 +0800

    1.4 done

---
+++ 1st_chapter/1.1.cpp
@@ -0,0 +1,22 @@
+//练习1.1 
+/*
+ * 编译器版本：
+ * g++ (Ubuntu 5.4.0-6ubuntu1~16.04.1) 5.4.0 20160609
+ * GNU文件命名约定：
+ * file.cc
+ * file.cp
+ * file.cxx
+ * file.cpp
+ * file.CPP
+ * file.c++
+ * file.C
+ *
+ */
+
+int main()
+{
+    return 0;		
+}
+
+
+
